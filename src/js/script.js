ad838a064c1353706abedfad79cb64a82c4ac6e7
dion <dion@levatine.btye2bytes.online>
2024-04-05 02:03:13 +0530

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,68 +1,35 @@
 $(document).ready(function() {
 
+    function toggleVisibility(textElement, isVisible) {
+        if (isVisible) {
+            textElement.css('visibility', 'visible');
+        } else {
+            textElement.css('visibility', 'hidden');
+        }
+    }
+
+    // Loop through each skill icon
     $('.skill').each(function() {
         var icon = $(this);
         var iconClasses = icon.attr('class');
 
-        if (iconClasses.includes('html')) {
-            var htmlText = $('.html').eq(1);
+        var textElement = $('.' + iconClasses.split(' ')[1]).eq(1);
 
-            icon.mouseover(function() {
-                htmlText.css('visibility', 'visible');
-            });
-            icon.mouseout(function() {
-                htmlText.css('visibility', 'hidden');
-            });
+        icon.hover(
+            function() {
+                toggleVisibility(textElement, true);
+            },
+            function() {
+                toggleVisibility(textElement, false);
+            }
+        );
+    });
 
-        } else if (iconClasses.includes('css')) {
-            var cssText = $('.css').eq(1);
-
-            icon.mouseover(function() {
-                cssText.css('visibility', 'visible');
-            });
-            icon.mouseout(function() {
-                cssText.css('visibility', 'hidden');
-            });
-
-        } else if (iconClasses.includes('js')) {
-            var jsText = $('.js').eq(1);
-
-            icon.mouseover(function() {
-                jsText.css('visibility', 'visible');
-            });
-            icon.mouseout(function() {
-                jsText.css('visibility', 'hidden');
-            });
-
-        } else if (iconClasses.includes('react')) {
-            var reactText = $('.react').eq(1);
-
-            icon.mouseover(function() {
-                reactText.css('visibility', 'visible');
-            });
-            icon.mouseout(function() {
-                reactText.css('visibility', 'hidden');
-            });
-
-        } else if (iconClasses.includes('node')) {
-            var nodeText = $('.node').eq(1);
-
-            icon.mouseover(function() {
-                nodeText.css('visibility', 'visible');
-            });
-            icon.mouseout(function() {
-                nodeText.css('visibility', 'hidden');
-            });
-
-        } else if (iconClasses.includes('git')) {
-            var gitText = $('.git').eq(1);
-
-            icon.mouseover(function() {
-                gitText.css('visibility', 'visible');
-            });
-            icon.mouseout(function() {
-                gitText.css('visibility', 'hidden');
-            });
-        }
+    // Handle click event for contact icons
+    $('.contact-icon').click(function(event) {
+        event.preventDefault(); 
+    
+        var link = $(this).attr('href');
+        window.open(link, '_blank');
     });
 });
